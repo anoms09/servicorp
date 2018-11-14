e31271f38828b826873aa018ebd5124e5b0ed623
Anoma Alade <aladeanoma@yahoo.com>
2018-11-14 10:53:02 +0100

    First commit

---
+++ index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>ServiCorp || Home</title>
+	<link href="./css/style.css" rel="stylesheet" type="text/css" media="screen"/>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+</head>
+<body>
+	<div class="row" id="header">
+			<div class="col-4">
+				<div style="float:left; margin-left:10px; margin-top:12px;">
+					<img src="./image/anomski.jpg" alt = "serviCorp" width="100px" height="80px"/>
+				</div>
+			</div>
+  			<div class="col-8">
+  				<div style="margin-top:12px; margin-left: 50px;">
+  					<h1> SERVICORP </h1>
+  				</div>
+  			</div>
+  	</div>
+		
+	<?php include("./php/nav.php"); ?>
+	<div id = "picture_Slide">
+		PICTURES
+	</div>
+	<div id = "content">
+		<div id="home_content">
+				<div style="text-align:center; margin-top:40px;">
+					<a class="textlink" href="#">
+					<button type="button" class="btn btn-success">REAL ESTATE</button></a>
+				</div>
+  		</div>
+  		<div id="home_content">
+				<div style="text-align:center; margin-top:40px;">
+					<a class="textlink" href="#">
+					<button type="button" class="btn btn-success">CAR POOLING</button></a>
+				</div>
+  		</div>
+  		<div id="home_content">
+				<div style="text-align:center; margin-top:40px;">
+					<a class="textlink" href="#">
+					<button type="button" class="btn btn-success">LOAN SERVICES</button></a>
+				</div>
+  		</div>
+  		<div id="home_content">
+				<div style="text-align:center; margin-top:40px;">
+					<a class="textlink" href="#">
+					<button type="button" class="btn btn-success">FORUM</button></a>
+				</div>
+  		</div>
+  		<div id="home_content">
+				<div style="text-align:center; margin-top:40px;">
+					<a class="textlink" href="#">
+					<button type="button" class="btn btn-success">MARKETING</button></a>
+				</div>
+  		</div>
+  		<div id="home_content">
+				<div style="text-align:center; margin-top:40px;">
+					<a class="textlink" href="#">
+					<button type="button" class="btn btn-success">JOBS AND PPA</button></a>
+				</div>
+  		</div>
+  		<div id="home_content">
+				<div style="text-align:center; margin-top:40px;">
+					<a class="textlink" href="#">
+					<button type="button" class="btn btn-success">PROFILE AND DM</button></a>
+				</div>
+  		</div>
+  		
+  		</div>
+	</div>
+	<div id="contact">
+		<a href="#">fb</a>
+		<a href="#">IG</a>
+		<a href="#">Twitter</a>
+	</div>
+	<div id = "footer">
+		Copyright &copy; 2018
+	</div>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</body>
+</html>
