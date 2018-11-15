1919c79c87a7fb338ac9404ee9c4e64703ebdcf4
Anoma Alade <aladeanoma@yahoo.com>
2018-11-15 10:30:54 +0100

    Carousel added

--- index.php
+++ index.php
@@ -22,7 +22,32 @@
 		
 	<?php include("./php/nav.php"); ?>
 	<div id = "picture_Slide">
-		PICTURES
+		<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+			<ol class="carousel-indicators">
+				<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+				<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+				<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+			</ol>
+			<div class="carousel-inner">
+				<div class="carousel-item active">
+					<img class="d-block w-100" src="./image/2.jpg" alt="First slide" width="800px" height="500px">
+				</div>
+				<div class="carousel-item">
+					<img class="d-block w-100" src="./image/3.jpg" alt="Second slide">
+				</div>
+				<div class="carousel-item">
+					<img class="d-block w-100" src="./image/anomski.jpg" alt="Third slide">
+				</div>
+			</div>
+			<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+				<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+				<span class="sr-only">Previous</span>
+			</a>
+			<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+				<span class="carousel-control-next-icon" aria-hidden="true"></span>
+				<span class="sr-only">Next</span>
+			</a>
+		</div>
 	</div>
 	<div id = "content">
 		<div id="home_content">
