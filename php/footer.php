7fab08973e38c0f2c267a82ab97b409d24e16421
Anoma Alade <aladeanoma@yahoo.com>
2018-11-17 08:50:44 +0100

    Added Form

---
+++ php/footer.php
@@ -0,0 +1,8 @@
+<div id="contact">
+		<a href="#">fb</a>
+		<a href="#">IG</a>
+		<a href="#">Twitter</a>
+	</div>
+	<div id = "footer">
+		Copyright &copy; 2018
+	</div>
--- php/nav.php
+++ php/nav.php
@@ -1,3 +1,17 @@
+<div class="row" id="header">
+      <div class="col-4">
+        <div style="float:left; margin-left:10px; margin-top:12px;">
+          <img src="./image/anomski.jpg" alt = "serviCorp" width="100px" height="80px"/>
+        </div>
+      </div>
+        <div class="col-8">
+          <div style="margin-top:12px; margin-left: 50px;">
+            <h1> SERVICORP </h1>
+          </div>
+        </div>
+    </div>
+
+
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
@@ -16,6 +30,11 @@
       <li class="nav-item">
         <a class="nav-link" href="#">Contact Us</a>
       </li>
+      <?php if (!isset($username)){ ?> 
+      <li class="nav-item">
+        <a class="nav-link" href="./login.php">Sign Up / Login</a>
+      </li>
+    <?php } ?>
     </ul>
   </div>
 </nav>
